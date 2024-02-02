6178ba4cb23da9a702d2769abde274ed564da168
suou <ultrajuice@hotmail.com>
2024-02-02 19:07:43 +0900

    10章完了

--- flask/scikit_learn_prj/app.py
+++ flask/scikit_learn_prj/app.py
@@ -1,7 +1,10 @@
-from flask import Flask, render_template
+from flask import (
+    Flask, render_template, request, flash, redirect, url_for
+    )
 import os
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 key = os.urandom(13)
@@ -31,6 +34,23 @@ def initialize_DB():
 def index():
     header = 'クラスタリングアプリケーション'
     return render_template('index.html', header=header)
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST':
+        name = request.form['name']
+        title = request.form['title']
+        file = request.files['file']
+        file_path = 'static/' + secure_filename(file.filename)
+        file.save(file_path)
+        register_data = Data(name=name, title=title, file_path=file_path)
+        db.session.add(register_data)
+        db.session.commit()
+        flash('アップロードに成功しました')
+        return redirect(url_for('index'))
+    else:
+        header = 'ファイルアップロード'
+        return render_template('upload.html', header=header)
 
 if __name__ == '__main__':
     app.run(debug=True)
