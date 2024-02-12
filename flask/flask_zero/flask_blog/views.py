c45eba82ce2c34fcd3be39b43c3668224b93dd72
suou <ultrajuice@hotmail.com>
2024-02-12 16:19:55 +0900

    20章完了

--- flask/flask_zero/flask_blog/views.py
+++ flask/flask_zero/flask_blog/views.py
@@ -4,22 +4,24 @@ from flask_blog import app
 @app.route('/')
 def show_entries():
     if not session.get('logged_in'):
-        return redirect('/login')
+        return redirect(url_for('login'))
     return render_template('entries/index.html')
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
         if request.form['username'] != app.config['USERNAME']:
-            print('ユーザー名が異なります.')
+            flash('ユーザー名が異なります.')
         elif request.form['password'] != app.config['PASSWORD']:
-            print('パスワードが異なります')
+            flash('パスワードが異なります')
         else:
             session['logged_in'] = True
-            return redirect('/')
+            flash('ログインしました')
+            return redirect(url_for('show_entries'))
     return render_template('entries/login.html')
     
 @app.route('/logout')
 def logout():
     session.pop('logged_in', None)
-    return redirect('/')
+    flash('ログアウトしました')
+    return redirect(url_for('show_entries'))
