0ead907ac656d12fce0c4587bbe66297b470a3b0
suou <ultrajuice@hotmail.com>
2024-02-19 22:52:21 +0900

    読本, bootstrap対応

--- django/oneday/blog/models.py
+++ django/oneday/blog/models.py
@@ -6,6 +6,8 @@ class Post(models.Model):
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    is_published = models.BooleanField(default=False)
+    image = models.ImageField(upload_to="uploads/", null=True, blank=True)
 
     def __str__(self):
         return self.title
--- django/oneday/blog/views.py
+++ django/oneday/blog/views.py
@@ -1,4 +1,5 @@
 from django.views.generic import ListView, DetailView
+from django.core.exceptions import PermissionDenied
 from blog.models import Post
 
 class PostList(ListView):
@@ -8,3 +9,10 @@ class PostList(ListView):
 class PostDetail(DetailView):
     model = Post
     context_object_name = "post"
+
+    def get_object(self):
+        post = super().get_object()
+        if post.is_published or self.request.user.is_authenticated:
+            return post
+        else:
+            raise PermissionDenied
