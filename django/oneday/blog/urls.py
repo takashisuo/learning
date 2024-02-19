767ae06c67f40fbf74ccc2ca2d7e90988072dc1c
suou <ultrajuice@hotmail.com>
2024-02-19 21:54:48 +0900

    2.8

--- django/oneday/blog/urls.py
+++ django/oneday/blog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from blog.views import PostList
+from blog.views import PostList, PostDetail
 
 urlpatterns = [
     path("", PostList.as_view(), name="post_list"),
+    path("post/<int:pk>/", PostDetail.as_view(), name="post_detail")
 ]
--- django/oneday/blog/views.py
+++ django/oneday/blog/views.py
@@ -1,6 +1,10 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from blog.models import Post
 
 class PostList(ListView):
     model = Post
-    
+    context_object_name = "posts"
+
+class PostDetail(DetailView):
+    model = Post
+    context_object_name = "post"
